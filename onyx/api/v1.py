948e665b819e193ed2fbd2130faa45cb15160a8e
Olivier Yiptong <olivier@olivieryiptong.com>
2014-04-17 11:23:47 -0400

    newtab-impression stub

--- onyx/api/v1.py
+++ onyx/api/v1.py
@@ -2,6 +2,13 @@ from flask import current_app, Blueprint, request, redirect, jsonify
 
 links = Blueprint('v1_links', __name__, url_prefix='/v1/links')
 
+@links.route('/newtab/impression', methods=['POST'])
+def newtab_impression():
+    """
+    Collect stats about newtab page impressions
+    """
+    return '', 201
+
 @links.route('/newtab', methods=['POST'])
 def newtab_serving():
     # redirecting 303 will hint to the client to change HTTP verb to GET
