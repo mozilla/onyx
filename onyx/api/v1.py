0ec73112acffc3730734e6efc06dfd21886c05d4
Olivier Yiptong <olivier@olivieryiptong.com>
2014-04-17 11:22:05 -0400

    click-counting method stub

--- onyx/api/v1.py
+++ onyx/api/v1.py
@@ -1,6 +1,14 @@
 from flask import current_app, Blueprint, request, redirect, jsonify
 
 links = Blueprint('v1_links', __name__, url_prefix='/v1/links')
+clicks = Blueprint('v1_clicks', __name__, url_prefix='/v1/clicks')
+
+@clicks.route('/newtab', methods=['POST'])
+def newtab_click():
+    """
+    Collect stats about clicks on the newtab page
+    """
+    return '', 201
 
 @links.route('/newtab', methods=['POST'])
 def newtab_serving():
@@ -9,3 +17,4 @@ def newtab_serving():
 
 def register_routes(app):
     app.register_blueprint(links)
+    app.register_blueprint(clicks)
