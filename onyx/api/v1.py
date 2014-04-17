ac5f03a0d60804441fa20e9ea79400c67bee7592
Olivier Yiptong <olivier@olivieryiptong.com>
2014-04-17 11:20:13 -0400

    newtab serving a sample static file

--- onyx/api/v1.py
+++ onyx/api/v1.py
@@ -1,27 +1,11 @@
 from flask import current_app, Blueprint, request, redirect, jsonify
 
-clicks = Blueprint('v1_clicks', __name__, url_prefix='/v1/clicks')
-
-@clicks.route('/newtab', methods=['POST'])
-def newtab_click():
-    """
-    Collect stats about clicks on the newtab page
-    """
-    return '', 201
-
 links = Blueprint('v1_links', __name__, url_prefix='/v1/links')
 
-@links.route('/newtab/impression', methods=['POST'])
-def newtab_impression():
-    """
-    Collect stats about newtab page impressions
-    """
-    return '', 201
-
-@links.route('/newtab', methods=['GET'])
+@links.route('/newtab', methods=['POST'])
 def newtab_serving():
-    return redirect(current_app.config['LINKS_LOCATION'])
+    # redirecting 303 will hint to the client to change HTTP verb to GET
+    return redirect(current_app.config['LINKS_LOCATION'], code=303)
 
 def register_routes(app):
     app.register_blueprint(links)
-    app.register_blueprint(clicks)
