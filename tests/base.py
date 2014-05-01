c8bb1a93506824d5d7e7de8020c47385cf76b626
Olivier Yiptong <olivier@olivieryiptong.com>
2014-05-01 16:16:54 -0400

    make tests run in test mode

--- tests/base.py
+++ tests/base.py
@@ -1,8 +1,10 @@
 from onyx.utils import environment_manager_create
+from onyx.default_settings import DefaultConfig
 from flask.ext.testing import TestCase
 
 class BaseTestCase(TestCase):
 
     def create_app(self):
+        DefaultConfig.ENVIRONMENT = 'test'
         self.app = environment_manager_create()
         return self.app
