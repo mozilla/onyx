f87adb737c7373ce40227b351316129e15b4695d
Olivier Yiptong <olivier@olivieryiptong.com>
2014-05-30 15:27:53 -0400

    re-add original runserver command for development

--- scripts/manage.py
+++ scripts/manage.py
@@ -4,7 +4,7 @@ from onyx.utils import environment_manager_create, GunicornServerCommand
 
 manager = Manager(environment_manager_create)
 manager.add_option('-c', '--config', dest='config', required=False)
-manager.add_command('runserver', GunicornServerCommand())
+manager.add_command('runserver_gunicorn', GunicornServerCommand())
 
 if __name__ == "__main__":
     manager.run()
