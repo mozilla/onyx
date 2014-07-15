607e37f538d507a20ccb624576f33cbb473a4694
Olivier Yiptong <olivier@olivieryiptong.com>
2014-07-15 11:53:08 -0400

    log the link fetching runtime via heka

--- onyx/api/v1.py
+++ onyx/api/v1.py
@@ -14,38 +14,42 @@ links = Blueprint('v1_links', __name__, url_prefix='/v1/links')
 
 
 @links.route('/newtab', methods=['POST'])
-# @onyx.hekalog.timer('newtab_serving')
 @capture_stack
 def newtab_serving():
     """
     Given a locale, return locale-specific links if possible.
     Set an identifier for a user if it isn't already set.
     """
-    try:
-        client_payload = request.get_json(cache=False)
-        locale = client_payload['locale']
-    except Exception:
-        onyx.hekalog.incr('payload_error', logger='watchlist', severity=3)
-        return Response('', content_type='application/json; charset=utf-8',
-                        status=400)
+    @onyx.hekalog.timer('newtab_serving')
+    def work():
+        try:
+            client_payload = request.get_json(cache=False)
+            locale = client_payload['locale']
+            directory_count = client_payload['directoryCount']
+        except Exception:
+            onyx.hekalog.incr('payload_error', logger='watchlist', severity=3)
+            return Response('', content_type='application/json; charset=utf-8',
+                            status=400)
 
-    localized = current_app.config['LINKS_LOCALIZATIONS'].get(locale)
+        localized = current_app.config['LINKS_LOCALIZATIONS'].get(locale)
 
-    response = None
+        response = None
 
-    if localized:
-        # 303 hints to the client to always use GET for the redirect
-        # ETag is handled by the directory link hosting server
-        response = make_response(redirect(localized, code=303))
-        onyx.hekalog.heka(type='serving', logger='payload_served',
-                          payload=json.dumps({
-                              'ip': request.remote_addr,
-                              'locale': locale
-                          }))
-    else:
-        response = make_response(('', 204))
+        if localized:
+            # 303 hints to the client to always use GET for the redirect
+            # ETag is handled by the directory link hosting server
+            response = make_response(redirect(localized, code=303))
+            onyx.hekalog.heka(type='serving', logger='payload_served',
+                              payload=json.dumps({
+                                  'ip': request.remote_addr,
+                                  'locale': locale,
+                                  'directoryCount': directory_count
+                              }))
+        else:
+            response = make_response(('', 204))
 
-    return response
+        return response
+    return work()
 
 
 def register_routes(app):
