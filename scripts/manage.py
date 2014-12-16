2d96c0688456b8ff53eb70cc1ab6a7df0cf93af2
Olivier Yiptong <olivier@olivieryiptong.com>
2014-12-16 15:57:00 -0500

    command to flood user events

--- scripts/manage.py
+++ scripts/manage.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 from flask.ext.script import Manager
-from onyx.utils import environment_manager_create, GunicornServerCommand
+from onyx.utils import environment_manager_create, GunicornServerCommand, LogCommand
 
 manager = Manager(environment_manager_create)
 manager.add_option('-c', '--config', dest='config', required=False)
 manager.add_command('runserver_gunicorn', GunicornServerCommand())
+manager.add_command('log', LogCommand)
 
 if __name__ == "__main__":
     manager.run()
