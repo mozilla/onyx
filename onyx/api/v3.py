66244f01397f3cd942daa61628ba462100fa16a3
Olivier Yiptong <olivier@olivieryiptong.com>
2015-04-20 11:40:38 -0400

    initial v3 api

---
+++ onyx/api/v3.py
@@ -0,0 +1,103 @@
+import logging
+from flask import (
+    Blueprint,
+    request,
+    make_response,
+    redirect,
+    Response
+)
+from onyx.environment import Environment
+from onyx.api.v2 import handle_ping
+
+
+links = Blueprint('v3_links', __name__, url_prefix='/v3/links')
+env = Environment.instance()
+
+
+@links.route('/fetch/<locale>/<channel>', methods=['GET'])
+@env.statsd.timer('v3_links_fetch')
+def fetch(locale=None, channel=None):
+    """
+    Given a locale, return locale-specific links if possible.
+    """
+    ip_addrs = None
+    ip_addr = None
+    ua = None
+
+    try:
+        ip_addrs = request.headers.get('X-Forwarded-For')
+        if ip_addrs is None:
+            ip_addrs = request.remote_addr
+
+        if ip_addrs is not None:
+            ip_addr = ip_addrs.split(',')[0]
+
+        ua = request.headers.get('User-Agent')
+
+    except Exception:
+        env.log_dict(name="client_error", action="fetch_malformed_payload", level=logging.WARN, message={
+            "ip": ip_addrs,
+            "ua": ua,
+            "locale": locale,
+            "channel": channel,
+            "ver": "3",
+        })
+        env.statsd.incr("fetch_error")
+        return Response('', content_type='application/json; charset=utf-8',
+                        status=400)
+
+    try:
+        country = env.geoip_db.country(ip_addr).country.iso_code
+    except:
+        country = "STAR"
+
+    localized = env.config.LINKS_LOCALIZATIONS.get("%s/%s" % (country, locale), {}).get('ag')
+    if localized is None:
+        localized = env.config.LINKS_LOCALIZATIONS.get("STAR/%s" % locale, {}).get('ag')
+
+    if localized is not None:
+        # 303 hints to the client to always use GET for the redirect
+        # ETag is handled by the directory link hosting server
+        response = make_response(redirect(localized, code=303))
+        env.log_dict(name="application", action="fetch_served", message={
+            "ip": ip_addrs,
+            "ua": ua,
+            "locale": locale,
+            "channel": channel,
+            "ver": "3",
+        })
+        env.statsd.incr("fetch_success")
+    else:
+        response = make_response(('', 204))
+        env.log_dict(name="application", action="fetch_locale_unavailable", message={
+            "ip": ip_addrs,
+            "ua": ua,
+            "locale": locale,
+            "channel": channel,
+            "ver": "3",
+        })
+        env.statsd.incr("fetch_locale_unavailable")
+
+    return response
+
+
+@links.route('/view', methods=['POST'])
+@env.statsd.timer('v3_links_view')
+def view():
+    """
+    Log impression ping sent from Firefox on each newtab open event
+    """
+    return handle_ping("view", api_version="3")
+
+
+@links.route('/click', methods=['POST'])
+@env.statsd.timer('v3_links_click')
+def click():
+    """
+    Log tile ping sent from Firefox on each tile action
+    """
+    return handle_ping("click", api_version="3")
+
+
+def register_routes(app):
+    app.register_blueprint(links)
