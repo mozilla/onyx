ad180a8b029bba7e3b6b8f4ae644bb7d74bbd4ac
Olivier Yiptong <olivier@olivieryiptong.com>
2015-04-28 16:30:00 -0700

    version bump to 1.4.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+1.4.1
+=====
+
+* Heartbeat API endpoint
+
 1.4.0
 =====
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="onyx",
-    version="1.4.0",
+    version="1.4.1",
     description="Link server and engagement metrics " +
                 "aggregator for Firefox Directory Links",
     author="Mozilla",
