1ee44d05d7210a0f3c5651a652e2c88475361d28
Olivier Yiptong <olivier@olivieryiptong.com>
2015-05-04 14:52:37 -0400

    Bug 1160366 - Remove response body on fetch redirect
    Squashed commit of the following:
    
    commit 213e0b0168ec4e4773cc00fd90a73add80f9243e
    Author: Olivier Yiptong <olivier@olivieryiptong.com>
    Date:   Mon May 4 12:52:03 2015 -0400
    
        make test for content_length 0 consistent across all API tests
    
    commit 3d8c7fb34aac943c86a8675e5b9b59f6c6c3f850
    Author: Olivier Yiptong <olivier@olivieryiptong.com>
    Date:   Thu Apr 30 16:25:14 2015 -0700
    
        make redirects return no body
    
    Closes bug 1160366
    Closes #17

--- tests/api/test_v1.py
+++ tests/api/test_v1.py
@@ -18,7 +18,7 @@ class TestNewtabServing(BaseTestCase):
                                     headers=[("User-Agent", "TestClient")])
         assert_equals(response.status_code, 400)
         assert_is_none(response.headers.get('Set-Cookie'))
-        assert_equals(int(response.headers.get('Content-Length')), 0)
+        assert_equals(response.content_length, 0)
 
     def test_missing_dircount(self):
         """
@@ -30,7 +30,7 @@ class TestNewtabServing(BaseTestCase):
                                     data=json.dumps({'locale': 'en-US'}))
         assert_equals(response.status_code, 400)
         assert_is_none(response.headers.get('Set-Cookie'))
-        assert_equals(int(response.headers.get('Content-Length')), 0)
+        assert_equals(response.content_length, 0)
 
     def test_unknown_locale(self):
         """
@@ -41,7 +41,7 @@ class TestNewtabServing(BaseTestCase):
                                     headers=[("User-Agent", "TestClient")],
                                     data=json.dumps({'locale': 'zh-CN', 'directoryCount': {"organic": 1}}))
         assert_equals(response.status_code, 204)
-        assert_equals(int(response.headers.get('Content-Length')), 0)
+        assert_equals(response.content_length, 0)
 
     def test_success(self):
         """
@@ -53,3 +53,4 @@ class TestNewtabServing(BaseTestCase):
                                     environ_base={"REMOTE_ADDR": "173.194.43.105"},
                                     data=json.dumps({'locale': 'en-US', 'directoryCount': {'organic': 1}}))
         assert_equals(response.status_code, 303)
+        assert_equals(response.content_length, 0)
